747bbf91e020bcc387da7ef0cfa996b03660f8f9
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-04-24 23:34:12 +0530

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect(url_for("home"))
+    return redirect(url_for("home")) # integrate with login page
 
 @app.route("/home")
 def home():
