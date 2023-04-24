9eab096b445b139f8c89304c1dab1d82dad533b2
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-04-24 23:19:22 +0530

    basic

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return redirect(url_for("home"))
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
