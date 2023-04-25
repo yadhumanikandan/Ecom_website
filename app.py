efc63611b8b788516772461351dcf14c11c6796f
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-04-25 22:23:25 +0530

    outline

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, session
 
 app = Flask(__name__)
+app.secret_key = "jsdklfj"
 
 @app.route("/")
 def index():
-    return redirect(url_for("home")) # integrate with login page
+    if "username" in session:
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("login"))
+    
+
 
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    if "username" in session:
+        return render_template("home.html", username = session["username"])
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if "username" in session:
+        return redirect(url_for("home"))
+    else:
+        if request.method == "POST":
+            session["username"] = request.form["username"]
+            session.permanent = True
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
 
 
 
